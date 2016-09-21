00a9810ce2d38b6a1eb3f3443c07ae853fb4baa7
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2016-09-21 14:28:15 +0200

    don't open dev tools on start

--- main.js
+++ main.js
@@ -11,9 +11,6 @@ function createWindow() {
     // and load the index.html of the app.
     win.loadURL(`file://${__dirname}/index.html`);
 
-    // Open the DevTools.
-    win.webContents.openDevTools();
-
     // Emitted when the window is closed.
     win.on('closed', () => {
         // Dereference the window object, usually you would store windows
